db69b274a8f8f0864c22fe2bf130eb467a4575c3
Sean Langton <slangton1@gmail.com>
2018-11-04 01:33:44 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -14,6 +14,7 @@ class Shoe
   end
 
   def BRANDS=(brand)
+    @brand = brand
     BRANDS << brand
   end
 
